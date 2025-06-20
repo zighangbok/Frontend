e245209396f8333629ce4aaed0f7d651b844fa2e
hyeonjin6530 <cola1876@naver.com>
2025-06-20 10:30:30 +0900

    chore: pretendard font 설정

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        pretendard: ['Pretendard', 'sans-serif'],
+      },
+      colors: {
+        primary: '#1E40AF',
+        secondary: '#F59E0B',
+      },
+    },
+  },
+  plugins: [],
+}
+
+export default config
