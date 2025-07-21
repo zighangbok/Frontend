37a8f793bece2e18d4d2c7fae8f8d193d3cc291a
hyeonjin6530 <cola1876@naver.com>
2025-07-21 15:10:28 +0900

    feat: 로그인 api 연동

--- src/components/join/login-form/index.tsx
+++ src/components/join/login-form/index.tsx
@@ -4,6 +4,9 @@ import { useRouter } from 'next/navigation';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { useState } from 'react';
+import { login } from '@/apis/join';
+import { toast } from 'sonner';
 
 interface LoginFormProps extends React.ComponentProps<'form'> {
   onSwitch?: () => void;
@@ -11,6 +14,24 @@ interface LoginFormProps extends React.ComponentProps<'form'> {
 
 export default function LoginForm({ onSwitch, ...props }: LoginFormProps) {
   const router = useRouter();
+
+  const [userId, setUserId] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      await login({ userId, password });
+      toast.success('로그인 성공');
+
+      // 로그인 성공시, 공고 페이지로 라우팅
+      router.push('/jobs');
+    } catch (error) {
+      console.error(error);
+      toast.error('로그인 실패. 잠시후 다시 시도해주세요');
+    }
+  };
 
   return (
     <form
@@ -23,19 +44,28 @@ export default function LoginForm({ onSwitch, ...props }: LoginFormProps) {
           <Input
             id="email"
             type="email"
-            placeholder="m@example.com"
+            placeholder="zighangbok@example.com"
             required
             className="h-12"
+            value={userId}
+            onChange={(e) => setUserId(e.target.value)}
           />
         </div>
         <div className="grid gap-3">
           <Label htmlFor="password">비밀번호</Label>
-          <Input id="password" type="password" required className="h-12" />
+          <Input
+            id="password"
+            type="password"
+            required
+            className="h-12"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <Button
           type="submit"
           className="h-12 w-full cursor-pointer"
-          onClick={() => router.push('/jobs')}
+          onClick={handleLogin}
         >
           로그인
         </Button>
