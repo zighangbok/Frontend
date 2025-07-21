ce5655931244199ab3536f02fac0e8abed038d9c
hyeonjin6530 <cola1876@naver.com>
2025-07-21 15:10:22 +0900

    feat: 회원가입 api 연동

--- src/components/join/register-form/index.tsx
+++ src/components/join/register-form/index.tsx
@@ -3,15 +3,34 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { useState } from 'react';
+import { signup } from '@/apis/join';
+import { toast } from 'sonner';
 
 interface RegisterFormProps extends React.ComponentProps<'form'> {
-  onSwitch?: () => void;
+  onSwitch: () => void;
 }
 
 export default function RegisterForm({
   onSwitch,
   ...props
 }: RegisterFormProps) {
+  const [userId, setUserId] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignUp = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      await signup({ userId, password });
+      toast.success('회원가입 성공');
+      onSwitch();
+    } catch (error) {
+      console.error(error);
+      toast.error('회원가입 실패. 잠시후 다시 시도해주세요');
+    }
+  };
+
   return (
     <form
       {...props}
@@ -26,13 +45,26 @@ export default function RegisterForm({
             placeholder="m@example.com"
             required
             className="h-12"
+            value={userId}
+            onChange={(e) => setUserId(e.target.value)}
           />
         </div>
         <div className="grid gap-3">
           <Label htmlFor="password">비밀번호</Label>
-          <Input id="password" type="password" required className="h-12" />
+          <Input
+            id="password"
+            type="password"
+            required
+            className="h-12"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
-        <Button type="submit" className="h-12 w-full cursor-pointer">
+        <Button
+          type="submit"
+          className="h-12 w-full cursor-pointer"
+          onClick={handleSignUp}
+        >
           회원가입
         </Button>
       </div>
