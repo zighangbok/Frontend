cc181ec77e25e880521704eeabb48b636181c79f
hyeonjin6530 <cola1876@naver.com>
2025-07-21 15:39:14 +0900

    refactor: 세션id를 사용함에 따라 불필요한 인터셉터 코드 삭제

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,17 +6,3 @@ export const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   withCredentials: true,
 });
-
-// 요청 인터셉터
-axiosInstance.interceptors.request.use(
-  (config) => {
-    // 쿠키에 토큰이 있다면 Authorization 헤더에 포함
-    const token =
-      typeof window !== 'undefined' ? getCookie('accessToken') : null;
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error),
-);
