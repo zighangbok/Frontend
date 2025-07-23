e4a99dec0fb32a55e3e50f590bc4c5ef11287b32
hyeonjin6530 <cola1876@naver.com>
2025-07-23 20:51:34 +0900

    feat: 시작 페이지를 /join으로 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/join', // 시작 페이지를 /join으로 설정
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
