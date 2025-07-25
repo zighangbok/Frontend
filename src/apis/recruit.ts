27ce1ae1789d9af9a06d53692d07966efa2e6032
Ready-Bridge <hanjungyo5533@gmail.com>
2025-07-25 23:41:19 +0900

    fix: session 로직 변경

--- src/apis/recruit.ts
+++ src/apis/recruit.ts
@@ -1,5 +1,6 @@
 import { axiosInstance } from '@/lib/axios';
 import { SimpleRecruit } from '@/types/recruit';
+import Cookies from 'js-cookie';
 
 // 일반 공고 불러오기
 export async function getRecruitmentSimpleList(
@@ -19,13 +20,28 @@ export async function getRecruitmentSimpleList(
 export async function getRecruitmentRecommendationList(): Promise<
   SimpleRecruit[]
 > {
-  const res = await axiosInstance.get('/recruitments/recommendations');
+  const userId = Cookies.get('userId');
+  if (!userId) {
+    throw new Error('User not logged in');
+  }
+
+  const res = await axiosInstance.get('/recruitments/recommendations', {
+    params: {
+      userId,
+    },
+  });
   return res.data;
 }
 
 // 공고 5회 클릭시 api 호출
 export async function postRecruitmentRerank(uuids: string[]) {
+  const userId = Cookies.get('userId');
+  if (!userId) {
+    throw new Error('User not logged in');
+  }
+
   const res = await axiosInstance.post('/recruitments/recommendations/rerank', {
+    userId,
     samples: uuids,
   });
   return res.data;
